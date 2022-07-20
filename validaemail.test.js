8271fcfda03c88a315bcc28f2fd2d4dd28d0abe7
fabio <fabiounmsm@gmail.com>
2022-07-20 13:27:29 -0500

    Actualizando

--- validaemail.test.js
+++ validaemail.test.js
@@ -5,6 +5,6 @@ test('email fabio.ticona@unmsm.edu.pe -> true',()=>{
 });
 
 test('email 1 -> false',()=>{
-    expect(ValidaEmail.isValidaEmail('1')).toBe(true) 
+    expect(ValidaEmail.isValidaEmail('1')).toBe(false) 
 }); 
 
